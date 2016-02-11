e1831577dc1315d778a69bb28f621153b67191ea
2091723f <2091723f@6dd607b9-cdc9-403c-abd1-38acbac064b5>
2016-02-11 12:34:33 +0000

    added living area procedure - requires renaming of SIMD local quintiles table - ticket 139
    git-svn-id: http://localhost:8082/extra/2015/tp3/r/repos/trunk@88 6dd607b9-cdc9-403c-abd1-38acbac064b5

---
+++ Database/procedures/getLivingAreaOutput.sql
@@ -0,0 +1,31 @@
+CREATE DEFINER=`ineqbench_user`@`%` PROCEDURE `getLivingAreaOutput`(IN start_age int(2), IN end_age int(2), IN sexIn VARCHAR(25), IN locality VARCHAR(25))
+BEGIN
+	SELECT
+	(SELECT
+	REPLACE(FORMAT(SUM(
+    (CASE WHEN locality = 'Dumfries & Galloway' then DumfriesGalloway
+             WHEN locality = 'Nithsdale' then Nithsdale
+             WHEN locality = 'Stewartry' then Stewartry
+             WHEN locality = 'Annandale & Eskdale' then AnnandaleEskdale
+             WHEN locality = 'Wigtownshire' then Wigtownshire
+        end)
+    )*
+        (SELECT SUM(PERCENT)/100 FROM POP_AGE_PERCENT WHERE AGE BETWEEN start_age AND end_age)*
+		(SELECT (PERCENT/100) FROM POP_GENDER_PERCENT WHERE GENDER=sexIn), 0) ,',','')
+		FROM SIMD_LOCAL_QUINTILES 
+        ) AS 'totalPopulation',    
+    
+	(SELECT
+	REPLACE(FORMAT(SUM(
+		(CASE WHEN locality = 'Dumfries & Galloway' then DumfriesGalloway
+             WHEN locality = 'Nithsdale' then Nithsdale
+             WHEN locality = 'Stewartry' then Stewartry
+             WHEN locality = 'Annandale & Eskdale' then AnnandaleEskdale
+             WHEN locality = 'Wigtownshire' then Wigtownshire
+        end)
+    )*
+        (SELECT SUM(PERCENT)/100 FROM POP_AGE_PERCENT WHERE AGE BETWEEN start_age AND end_age)*
+		(SELECT (PERCENT/100) FROM POP_GENDER_PERCENT WHERE GENDER=sexIn), 0) ,',','')
+		FROM SIMD_LOCAL_QUINTILES 
+        WHERE QUINTILE < 3) AS 'totalDeprived';
+END
