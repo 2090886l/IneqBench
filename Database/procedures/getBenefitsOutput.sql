6b5a858888025d3a96a6bf0aac31873aa5d1f580
2091723f <2091723f@6dd607b9-cdc9-403c-abd1-38acbac064b5>
2016-02-11 11:55:36 +0000

    added geography to procedures - ticket 153
    git-svn-id: http://localhost:8082/extra/2015/tp3/r/repos/trunk@87 6dd607b9-cdc9-403c-abd1-38acbac064b5

--- Database/procedures/getBenefitsOutput.sql
+++ Database/procedures/getBenefitsOutput.sql
@@ -1,17 +1,28 @@
 DELIMITER ;;
-CREATE DEFINER=`ineqbench_user`@`%` PROCEDURE `getBenefitsOutput`(IN start_age int(2), IN end_age int(2), IN sexIn VARCHAR(25))
+CREATE DEFINER=`ineqbench_user`@`%` PROCEDURE `getBenefitsOutput`(IN start_age int(2), IN end_age int(2), IN sexIn VARCHAR(25), IN locality VARCHAR(25))
 BEGIN
 SELECT (
 	SELECT REPLACE(FORMAT(SUM(ALL_PEOPLE)*(SELECT  SUM(PERCENT)/100
 		FROM POP_AGE_PERCENT
 		WHERE AGE BETWEEN start_age AND end_age),0),',','')
-		FROM ETHNIC_GROUP
+		FROM ETHNIC_GROUP as e
 		WHERE SEX = sexIn
-		AND AGE != 'Total') AS 'totalPopulation',
+		AND AGE != 'Total'
+			AND( locality = "Dumfries & Galloway" 
+			OR POSTCODE in (SELECT SUBSTRING(Postcode,1, LENGTH( e.POSTCODE )) 
+				FROM GEOGRAPHY_LOOKUP WHERE locality = GEOGRAPHY_LOOKUP.Locality)  )
+        ) AS 'totalPopulation',
         
     (SELECT
-		REPLACE(FORMAT( BENEFITS *
-        (SELECT SUM(PERCENT)/100 FROM POP_AGE_PERCENT WHERE AGE BETWEEN start_age AND end_age),0),',','')
+		REPLACE(FORMAT( BENEFITS 
+        *( 
+		(SELECT SUM(ALL_PEOPLE) FROM ETHNIC_GROUP as e2 WHERE SEX=sexIN AND AGE != 'Total' AND 
+			( locality = "Dumfries & Galloway" 
+				OR POSTCODE in (SELECT SUBSTRING(Postcode,1, LENGTH( e2.POSTCODE )) 
+					FROM GEOGRAPHY_LOOKUP WHERE locality = GEOGRAPHY_LOOKUP.Locality)) )
+		/ (SELECT SUM(ALL_PEOPLE) FROM ETHNIC_GROUP WHERE SEX = sexIN AND AGE != 'Total')
+		)
+        *(SELECT SUM(PERCENT)/100 FROM POP_AGE_PERCENT WHERE AGE BETWEEN start_age AND end_age),0),',','')
 		FROM BENEFIT_TOTALS WHERE GENDER=sexIn) AS 'totalDeprived';
 END ;;
 DELIMITER ;
--- Database/procedures/getEthnicOutput.sql
+++ Database/procedures/getEthnicOutput.sql
@@ -1,21 +1,27 @@
 DELIMITER ;;
-CREATE DEFINER=`ineqbench_user`@`%` PROCEDURE `getEthnicOutput`(IN start_age int(2), IN end_age int(2), IN sexIn VARCHAR(25))
+CREATE DEFINER=`ineqbench_user`@`%` PROCEDURE `getEthnicOutput`(IN start_age int(2), IN end_age int(2), IN sexIn VARCHAR(25), IN locality VARCHAR(25))
 BEGIN
 SELECT (
 	SELECT REPLACE(FORMAT(SUM(ALL_PEOPLE)*(SELECT  SUM(PERCENT)/100
 FROM POP_AGE_PERCENT
 WHERE AGE BETWEEN start_age AND end_age),0),',','')
-	FROM ETHNIC_GROUP
+	FROM ETHNIC_GROUP AS e
 	WHERE SEX = sexIn
-	AND AGE != 'Total') AS 'totalPopulation',
+	AND AGE != 'Total'
+		AND( locality = "Dumfries & Galloway" 
+		OR POSTCODE in (SELECT SUBSTRING(Postcode,1, LENGTH( e.POSTCODE )) FROM GEOGRAPHY_LOOKUP WHERE locality = GEOGRAPHY_LOOKUP.Locality)  )
+    ) AS 'totalPopulation',
 
 (SELECT FORMAT((SUM(WHITE_TRAVELLER) + SUM(ASIAN_PAKISTANI) + SUM(ASIAN_INDIAN) + 
 SUM(ASIAN_BANGLADESHI) + SUM(CARIBBEAN_BRITISH) +
 SUM(CARIBBEAN_BLACK))*(SELECT  SUM(PERCENT)/100
 FROM POP_AGE_PERCENT
 WHERE AGE BETWEEN start_age AND end_age),0)
-FROM ETHNIC_GROUP
+FROM ETHNIC_GROUP AS e
 WHERE SEX = sexIn
-AND AGE != 'Total') AS 'totalDeprived';
+AND AGE != 'Total'
+	AND( locality = "Dumfries & Galloway" 
+	OR POSTCODE in (SELECT SUBSTRING(Postcode,1, LENGTH( e.POSTCODE )) FROM GEOGRAPHY_LOOKUP WHERE locality = GEOGRAPHY_LOOKUP.Locality)  )
+) AS 'totalDeprived';
 END ;;
 DELIMITER ;
