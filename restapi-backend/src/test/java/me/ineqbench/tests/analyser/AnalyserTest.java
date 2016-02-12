7b00670c01471ba217caf53f40735d279519f603
2087690c <2087690c@6dd607b9-cdc9-403c-abd1-38acbac064b5>
2016-02-12 23:33:41 +0000

    Test Analyser - this is the class estimating the expecting value for the benchmarking - core test! - ticket #184
    git-svn-id: http://localhost:8082/extra/2015/tp3/r/repos/trunk@99 6dd607b9-cdc9-403c-abd1-38acbac064b5

---
+++ restapi-backend/src/test/java/me/ineqbench/tests/analyser/AnalyserTest.java
@@ -0,0 +1,36 @@
+package me.ineqbench.tests.analyser;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import me.ineqbench.analyst.Analyser;
+import me.ineqbench.clientResponsePojos.ClientResponsePOJO;
+import me.ineqbench.dbResponsePOJOs.ResponseTuplePOJO;
+
+public class AnalyserTest {
+
+	private ResponseTuplePOJO dbResponse;
+	private int numberOfPeople;
+	
+	private ClientResponsePOJO clientExpResponse;
+	@Before
+    public void setUp() {
+		dbResponse = new ResponseTuplePOJO(50000, 200);
+		numberOfPeople = 500;
+		clientExpResponse =  new ClientResponsePOJO(50000, 200, 7.2330, 0.5488, 2);
+	}
+	
+	@Test
+   public void testSalutationMessage() {
+      ClientResponsePOJO estimates = Analyser.getEstimate(dbResponse, numberOfPeople);
+      
+      // Add epsilons as asserEquals(String,double,double) has been deprecated
+      assertEquals("Expects  for Estimate",estimates.getEstimate(),clientExpResponse.getEstimate(),0.5);
+      assertEquals("Expects  for Lower Range",estimates.getLowerRange(),clientExpResponse.getLowerRange(),0.2);
+      assertEquals("Expects  for Upper Range",estimates.getUpperRange(),clientExpResponse.getUpperRange(),0.2);
+      assertEquals("Expects  for Total Deprived",estimates.getTotalDeprived(),clientExpResponse.getTotalDeprived());
+      assertEquals("Expects  for Total Population",estimates.getTotalPopulation(),clientExpResponse.getTotalPopulation());
+   }
+}
+
