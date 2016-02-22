bd3f1d8266b83eda1b08985a8817a50e9734d48e
2093693r <2093693r@6dd607b9-cdc9-403c-abd1-38acbac064b5>
2016-02-22 18:14:13 +0000

    Refactored the code in the main controller.
    git-svn-id: http://localhost:8082/extra/2015/tp3/r/repos/trunk@113 6dd607b9-cdc9-403c-abd1-38acbac064b5

--- frontEnd/angular/mainController.js
+++ frontEnd/angular/mainController.js
@@ -1,50 +1,46 @@
-var app = angular.module('IneqBenchControllers',[]);
+var app = angular.module('IneqBenchControllers', []);
 
 
-app.controller('MainController',["$http","$scope",function($http,$scope){
+app.controller('MainController', ["$http", "$scope", function($http, $scope) {
 
     var url = "http://localhost:8080";
-    //System state
-    $scope.showSelectSection = false;
-    $scope.showDeprivationCriteria = false;
-    $scope.showPopulationVariables = false;
-    $scope.showOutputType = false;
-    $scope.isVisualizing = false;
-    $scope.wrapper = {}; // use one popVariable object wrapping all the data
-    // used because ng-if creates DOM dynamically and Angular cannot find assigned values
-    $scope.wrapper.mode = null;//this is either benchmark or predict
-    $scope.wrapper.isBenchmarkingCheckedPopVar = false;
-    $scope.wrapper.isPredictingCheckedPopVar = false;
-    $scope.wrapper.ageRange=null;
-    $scope.isBenchmarkingCheckedIneq = false;
-    $scope.isPredictingCheckedIneq = false;
-    //System state end
-    //Population variables
-    $scope.wrapper.numberOfPeople = null;
-    $scope.wrapper.region=null;
-    $scope.wrapper.gender=null;
-    $scope.isAllGenderCheckedIneq = false;
-    $scope.isMaleGenderCheckedIneq = false;
-    $scope.isFemaleGenderChecked = false;
-    //Population variables end
-    //Deprivation Criteria
-    $scope.wrapper.deprivationCriteria=null;
-    //Deprivation Criteria end
 
+    $scope.showBenchmarking = false;
+    $scope.showVisualizing = false;
 
-
-    //Output type Section Params
-    $scope.visualization=null;
-    $scope.isBenchmarkingCheckedOutput = false;
-    $scope.isPredictingCheckedOutput = false;
-    $scope.isAllGenderCheckedOutPut = false;
-    $scope.isMaleGenderCheckedOutPut = false;
-    $scope.isFemaleGenderCheckedOutPut = false;
-    //Output type Section Params end
-
+    $scope.numberOfPeople = null;
+    $scope.ageRange = null;
+    $scope.region = null;
+    $scope.gender = null;
 
     $scope.selectedDeprivations = {};
     $scope.results = {};
+
+    $scope.benchmark = function() {
+      if (!$scope.showBenchmarking)
+        $scope.showBenchmarking = true;
+    };
+
+    // Visualize button event -> when button clicked, find which criterion is selected and call the corresponding HTTP GET Restful request
+    $scope.visualize = function() {
+      $scope.showVisualizing = true;
+      for (deprivation in $scope.selectedDeprivations) {
+        getData($scope.selectedDeprivations[deprivation]['str'],
+                $scope.numberOfPeople, $scope.ageRange.rangeInt,
+                $scope.gender, $scope.region.name
+        );
+      }
+    };
+
+    $scope.clearSearch = function() {
+      $scope.results = null;
+      $scope.selectedDeprivations = null;
+      $scope.ageRange = null;
+      $scope.numberOfPeople = null;
+      $scope.region = null;
+      $scope.gender = null;
+      $scope.showVisualizing = false;
+    };
 
     // Add/remove a deprivation from the list of selected deprivations.
     $scope.updateSelectedDeprivations = function(deprivation) {
@@ -75,11 +71,11 @@ app.controller('MainController',["$http","$scope",function($http,$scope){
           "&ageGroup=" + ageGroup +
           "&gender=" + gender +
           "&locality=" + locality;
+      $scope.url = urlStr;
       $http.get(urlStr).success(function(data){
-          setResult(data, deprivation);
+        setResult(data, deprivation);
       });
     };
-
 
     //Mappings
     //Deprivation Criterion params to names mapping
@@ -109,6 +105,7 @@ app.controller('MainController',["$http","$scope",function($http,$scope){
         {name:'Wigtownshire'},
         {name:'Dumfries & Galloway'}
     ];
+
     $scope.ranges=[
         {rangeStr:"0-10",rangeInt:[0,10]},
         {rangeStr:"10-20",rangeInt:[10,20]},
@@ -122,126 +119,22 @@ app.controller('MainController',["$http","$scope",function($http,$scope){
         {rangeStr:"90-100",rangeInt:[90,100]},
         {rangeStr:"100-110",rangeInt:[100,110]}
     ];
-    //Data end
-
-    //Mappings End
 
     //Miscellaneous
     function setResult(data, daprivation){
-        data = roundData(data);
-        $scope.results[deprivation] = {data: data, name: deprivationCriteriasNames[deprivation]};
+      data = roundData(data);
+      $scope.results[deprivation] = {data: data, name: deprivationCriteriasNames[deprivation]};
     }
     function roundData(data){
-        data.totalPopulation=Math.round(data.totalPopulation);
-        data.totalDeprived=Math.round(data.totalDeprived);
-        data.upperRange=Math.round(data.upperRange);
-        data.lowerRange=Math.round(data.lowerRange);
-        data.estimate=Math.round(data.estimate);
-        data.totalDeprivedPercentage=Math.round((data.totalDeprived/data.totalPopulation)*100);
-        data.estimatePercentage=Math.round((data.estimate/$scope.wrapper.numberOfPeople)*100);
-        data.lowerRangePercentage=Math.round((data.lowerRange/$scope.wrapper.numberOfPeople)*10000)/100;
-        data.upperRangePercentage=Math.round((data.upperRange/$scope.wrapper.numberOfPeople)*10000)/100;
-        return data;
+      data.totalPopulation=Math.round(data.totalPopulation);
+      data.totalDeprived=Math.round(data.totalDeprived);
+      data.upperRange=Math.round(data.upperRange);
+      data.lowerRange=Math.round(data.lowerRange);
+      data.estimate=Math.round(data.estimate);
+      data.totalDeprivedPercentage=Math.round((data.totalDeprived/data.totalPopulation)*100);
+      data.estimatePercentage=Math.round((data.estimate/$scope.wrapper.numberOfPeople)*100);
+      data.lowerRangePercentage=Math.round((data.lowerRange/$scope.wrapper.numberOfPeople)*10000)/100;
+      data.upperRangePercentage=Math.round((data.upperRange/$scope.wrapper.numberOfPeople)*10000)/100;
+      return data;
     }
-
-    function nullifyAllViewShowParams(){
-        $scope.ethincityDeprivationViewShow = false;
-        $scope.taxBandViewShow = false;
-        $scope.unpaidCarersViewShow = false;
-        $scope.learningDisabilitiesViewShow = false;
-        $scope.educationalAttainmentViewShow = false;
-        $scope.transportViewShow = false;
-        $scope.unemployedViewShow = false;
-        $scope.homelessViewShow = false;
-        $scope.lowPayViewShow = false;
-        $scope.offendersViewShow = false;
-        $scope.mentalHealthAndWellbeingViewShow = false;
-        $scope.fuelPovertyViewShow = false;
-        $scope.illnessViewShow = false;
-    }
-    //End Miscellaneous
-
-    //Events
-
-    //if the benchmark button is clicked show/hide the correct sections
-    //for more info see parameter names
-    $scope.benchmarkClicked = function(){
-        $scope.showSelectSection = true;
-        $scope.showDeprivationCriteria = false;
-        $scope.showPopulationVariables = false;
-        $scope.showOutputType = false;
-        $scope.showDeprivationCriteria = true;
-        $scope.wrapper.isBenchmarkingCheckedPopVar = true;
-        $scope.wrapper.mode = "benchmarking"; //needed to set the mode, it is not set automatically from ng-model as checked state
-        //set  programmatically
-    };
-
-    //if the back button is clicked show/hide the correct sections
-    //for more info see parameter names
-    $scope.backToTop = function(){
-        $scope.showDeprivationCriteria = false;
-        $scope.showPopulationVariables = false;
-        $scope.showOutputType = false;
-        $scope.ethincityDeprivationViewShow = false;
-        $scope.taxBandViewShow = false;
-        $scope.unpaidCarersViewShow = false;
-        $scope.learningDisabilitiesViewShow = false;
-        $scope.educationalAttainmentViewShow = false;
-        $scope.transportViewShow = false;
-        $scope.unemployedViewShow = false;
-        $scope.homelessViewShow = false;
-        $scope.lowPayViewShow = false;
-        $scope.offendersViewShow = false;
-        $scope.mentalHealthAndWellbeingViewShow = false;
-        $scope.fuelPovertyViewShow = false;
-        $scope.illnessViewShow = false;
-        $scope.isVisualizing = false;
-        $scope.showSelectSection = false;
-    };
-
-    //if all fields in the Deprivation View are selected ->
-    //proceed with the Population View (hide Deprivation and show Population view)
-    $scope.doesProceedDeprivationParameters = function(){
-        if ($scope.wrapper.deprivationCriteria!=null && $scope.wrapper.numberOfPeople!=null){
-            $scope.showDeprivationCriteria = false;
-            $scope.showPopulationVariables = true;
-
-            $scope.wrapper.isBenchmarkingCheckedPopVar = false;
-            $scope.wrapper.isPredictingCheckedPopVar = false;
-        }
-    };
-
-    // Visualize button event -> when button clicked, find which criterion is selected and call the corresponding HTTP GET Restful request
-    $scope.visualize = function(){
-        $scope.isVisualizing = true;
-        for (deprivation in $scope.selectedDeprivations) {
-            getData($scope.selectedDeprivations[deprivation]['str'],
-                    $scope.wrapper.numberOfPeople, $scope.wrapper.ageRange.rangeInt,
-                    $scope.wrapper.gender, $scope.wrapper.region.name
-            );
-        }
-    };
-
-    //Setting params for gender mapping gender selected to gender param
-    $scope.genderClicked = function (){
-        if ($scope.wrapper.gender=="All People:")
-            $scope.isAllGenderCheckedIneq =true;
-        if ($scope.wrapper.gender=="Males:")
-            $scope.isMaleGenderCheckedIneq=true;
-        if ($scope.wrapper.gender=="Females:")
-            $scope.isFemaleGenderChecked =true;
-        $scope.doesProceedDeprivationParameters();
-    };
-
-    // Post request and get request examples
-
-    //$http.post("/api/addCustomer", {name:"alex",age:"21"}).success(function(data) {
-    //    $scope.hello = data;
-    //    console.log(data);
-    //})
-    //$http.get('/api/getCustomerById?id=2').success(function(data){
-    //    console.log(data);
-    //});
-
-
 }]);
