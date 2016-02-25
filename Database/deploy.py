6e5d97b1aa6a97df9d4e51ebe1c36ea4a2255d91
2094613s <2094613s@6dd607b9-cdc9-403c-abd1-38acbac064b5>
2016-02-25 15:11:49 +0000

    Modified deploy.py for sql deployment so that it uses standard output
    git-svn-id: http://localhost:8082/extra/2015/tp3/r/repos/trunk@125 6dd607b9-cdc9-403c-abd1-38acbac064b5

--- Database/deploy.py
+++ Database/deploy.py
@@ -2,17 +2,13 @@ import os
 
 folders = [('schema',False),('data',False),('functions',True),('procedures',True)]
 
-f = open('toRun2.sql','w')
-f.write('USE ineq_bench;\n')
+print('USE ineq_bench;\n')
 
 for eachfolder in folders: 
 	for each in os.listdir(eachfolder[0]): 
-		print each + ' - ' + str(eachfolder[1])
 		if(eachfolder[1]): 
-			f.write('DELIMITER $$\nUSE \'ineq_bench\'$$\n')
+			print('DELIMITER $$\nUSE \'ineq_bench\'$$\n')
 		with open(os.path.join(eachfolder[0],each), 'r') as content_file:
-			f.write(content_file.read())
+			print(content_file.read())
 		if(eachfolder[1]):
-			f.write('$$ \nDELIMITER ;\n')
-
-f.close()
+			print('$$ \nDELIMITER ;\n')
