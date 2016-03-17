59864f0c4590de6bef4f9a80accab8c73b7b0a67
2091723f <2091723f@6dd607b9-cdc9-403c-abd1-38acbac064b5>
2016-03-17 14:52:51 +0000

    edited getTaxOutput procedure to accurately return total population in number of households rather than of people - ticket 294
    git-svn-id: http://localhost:8082/extra/2015/tp3/r/repos/trunk@177 6dd607b9-cdc9-403c-abd1-38acbac064b5

--- Database/procedures/getTaxOutput.sql
+++ Database/procedures/getTaxOutput.sql
@@ -1,17 +1,25 @@
 DROP PROCEDURE IF EXISTS getTaxOutput;
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getTaxOutput`(IN start_age int(2), IN end_age int(2), IN sexIn VARCHAR(25), IN locality VARCHAR(25))
 BEGIN
 /* procedure for getting those living in A or B tax band houses
-gets totalPopulation using the totalPopulation function
-and totalDeprived from LONG_TERM_HEALTH_CONDITION
+gets totalPopulation from summing the totals for all households
+and totalDeprived from TAX_BAND
+NOTE: THIS RETURNS VALUES IN TERMS OF HOUSEHOLDS, NOT PEOPLE
 */
 SELECT (
-	SELECT totalPopulation(start_age,end_age,sexIn,locality)) AS 'totalPopulation',
-	(SELECT REPLACE( FORMAT ( ( SUM(A)+SUM(B) )*
+	SELECT REPLACE( FORMAT ( ( SUM(A)+SUM(B)+SUM(C)+SUM(D)+SUM(E)+SUM(F)+SUM(G)+SUM(H) )*
         (SELECT SUM(PERCENT)/100 FROM POP_AGE_PERCENT WHERE AGE BETWEEN start_age AND end_age)* -- estimate age group size using POP_AGE_PERCENT
 		(SELECT (PERCENT/100) FROM POP_GENDER_PERCENT WHERE GENDER=sexIn), 0) ,',','') -- estimate gender group size using POP_GENDER_PERCENT
 		FROM TAX_BAND
-        WHERE (locality = "Dumfries & Galloway" -- if the locality is D&G we don't need to filter by locality using GEOGRAPHY_LOOKUP postcodes
+			WHERE (locality = "Dumfries & Galloway" 
+			OR POST_CODE in (SELECT Postcode FROM GEOGRAPHY_LOOKUP WHERE locality = GEOGRAPHY_LOOKUP.Locality)  )
+        ) AS 'totalPopulation',
+	(SELECT REPLACE( FORMAT ( ( SUM(A)+SUM(B) )*
+        (SELECT SUM(PERCENT)/100 FROM POP_AGE_PERCENT WHERE AGE BETWEEN start_age AND end_age)*  -- estimate age group size using POP_AGE_PERCENT
+		(SELECT (PERCENT/100) FROM POP_GENDER_PERCENT WHERE GENDER=sexIn), 0) ,',','')  -- estimate gender group size using POP_GENDER_PERCENT
+		FROM TAX_BAND
+        WHERE (locality = "Dumfries & Galloway" 
 			OR POST_CODE in (SELECT Postcode FROM GEOGRAPHY_LOOKUP WHERE locality = GEOGRAPHY_LOOKUP.Locality)  )
         ) AS 'totalDeprived';
 END
